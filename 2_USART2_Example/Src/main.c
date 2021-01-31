71cdf02ab4f2e78eaf4efaf6c39e8272faf53528
alejandro-cp95 <alejandro_cp95@outlook.com>
2021-01-31 13:31:02 -0600

    2_1. Anteriormente se hicieron configuraciones de bajo nivel de procesador, ahora se hacen del periférico, así como sus respectivas configuraciones de alto nivel

--- 2_USART2_Example/Src/main.c
+++ 2_USART2_Example/Src/main.c
@@ -8,16 +8,41 @@
 #include "stm32f3xx_hal.h"
 #include "main.h"
 
+UART_HandleTypeDef huart2;
+
 void SystemClockConfig(void);
+void UART2_Init(void);
+void Error_handler(void);
 
 int main()
 {
 	HAL_Init();
 	SystemClockConfig();
+	UART2_Init();
 	return 0;
 }
 
 void SystemClockConfig(void)
+{
+
+}
+
+void UART2_Init(void)
+{
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	if(HAL_UART_Init(&huart2) != HAL_OK)
+	{
+		Error_handler();
+	}
+}
+
+void Error_handler(void)
 {
 
 }
--- 2_USART2_Example/Src/msp.c
+++ 2_USART2_Example/Src/msp.c
@@ -18,3 +18,20 @@ void HAL_MspInit(void)
 	HAL_NVIC_SetPriority(BusFault_IRQn,0,0);
 	HAL_NVIC_SetPriority(UsageFault_IRQn,0,0);
 }
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+	GPIO_InitTypeDef uart2_gpio;
+	// Enable clock for the USART2 peripheral
+	__HAL_RCC_USART2_CLK_ENABLE();
+	// Pin muxing configurations
+	uart2_gpio.Alternate = GPIO_AF7_USART2;
+	uart2_gpio.Mode = GPIO_MODE_AF_PP;
+	uart2_gpio.Pin = GPIO_PIN_2;
+	uart2_gpio.Pull = GPIO_PULLUP;
+	uart2_gpio.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOA, &uart2_gpio);
+	// Enable IRQ and set priority
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+	HAL_NVIC_SetPriority(USART2_IRQn,15,0);
+}
